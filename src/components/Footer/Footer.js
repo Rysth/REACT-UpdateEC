7c32585cdf6d0558a8c828ab2016b796ea795a52
Rysth <johnpalacios.t@gmail.com>
2023-09-11 10:11:55 -0500

    Added branch name into the footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,88 +11,101 @@ function Footer() {
       transition={{ duration: 2, type: 'spring' }}
       className="footer bg-white"
     >
-      <div className="container md:container mx-auto mt-10 md:mt-20 p-4 py-8 md:py-32 flex flex-wrap gap-10 lg:gap-20 2xl:justify-between">
-        <div className="bg-gray-900 w-40 md:w-60 p-2 px-6 md:py-4 rounded-lg">
-          <Link to="/" className="w">
-            <img className="w-full h-full" src={Logo} alt="" />
-          </Link>
+      <div className="container md:container mx-auto mt-10 md:mt-20 p-4 py-8 md:py-32 md:pb-16">
+        <div className="flex flex-wrap gap-10 lg:gap-20 2xl:justify-between">
+          <div className="bg-black w-40 md:w-60 p-2 px-6 md:py-4 rounded-lg">
+            <Link to="/" className="w">
+              <img className="w-full h-full" src={Logo} alt="" />
+            </Link>
+          </div>
+          <ul className="flex flex-col p-0 gap-2">
+            <li className="text-sm md:text-base mb-2">
+              <h3 className="font-bold">Términos y Condiciones</h3>
+            </li>
+            <li className="text-xs md:text-base text-gray-900">
+              <a href="/" className="md:hover:underline">
+                Términos de Servicio
+              </a>
+            </li>
+            <li className="text-xs md:text-base text-gray-900">
+              <a href="/" className="md:hover:underline">
+                Política de Privacidad
+              </a>
+            </li>
+          </ul>
+          <ul className="flex flex-col p-0 gap-2">
+            <li className="text-sm md:text-base mb-2">
+              <h3 className="font-bold">Contactos</h3>
+            </li>
+            <li className="text-xs md:text-base text-gray-900">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="tel:+593984798317"
+                className="md:hover:underline"
+              >
+                (+593) 0984798317
+              </a>
+            </li>
+            <li className="text-xs md:text-base text-gray-900">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="mailto:update.ec.gye@gmail.com"
+                className="md:hover:underline"
+              >
+                update.ec.gye@gmail.com
+              </a>
+            </li>
+          </ul>
+          <ul className="flex flex-col p-0 gap-2">
+            <li className="text-sm md:text-base mb-2">
+              <h3 className="font-bold">Compañia</h3>
+            </li>
+            <li className="text-xs md:text-base text-gray-900">
+              <a href="/" className="md:hover:underline">
+                Inicio
+              </a>
+            </li>
+            <li className="text-xs md:text-base text-gray-900">
+              <a href="/" className="md:hover:underline">
+                Nosotros
+              </a>
+            </li>
+            <li className="text-xs md:text-base text-gray-900">
+              <a href="/" className="md:hover:underline">
+                Contáctanos
+              </a>
+            </li>
+          </ul>
+          <ul className="flex flex-col p-0 gap-2">
+            <li className="text-sm md:text-base mb-2">
+              <h3 className="font-bold">Redes Sociales</h3>
+            </li>
+            <li className="text-xs md:text-base text-gray-900">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/update.ec/"
+                className="md:hover:underline"
+              >
+                Instagram
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul className="flex flex-col p-0 gap-2">
-          <li className="text-sm md:text-base mb-2">
-            <h3 className="font-bold">Términos y Condiciones</h3>
-          </li>
-          <li className="text-xs md:text-base text-gray-900">
-            <a href="/" className="md:hover:underline">
-              Términos de Servicio
-            </a>
-          </li>
-          <li className="text-xs md:text-base text-gray-900">
-            <a href="/" className="md:hover:underline">
-              Política de Privacidad
-            </a>
-          </li>
-        </ul>
-        <ul className="flex flex-col p-0 gap-2">
-          <li className="text-sm md:text-base mb-2">
-            <h3 className="font-bold">Contactos</h3>
-          </li>
-          <li className="text-xs md:text-base text-gray-900">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="tel:+593984798317"
-              className="md:hover:underline"
-            >
-              (+593) 0984798317
-            </a>
-          </li>
-          <li className="text-xs md:text-base text-gray-900">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="mailto:update.ec.gye@gmail.com"
-              className="md:hover:underline"
-            >
-              update.ec.gye@gmail.com
-            </a>
-          </li>
-        </ul>
-        <ul className="flex flex-col p-0 gap-2">
-          <li className="text-sm md:text-base mb-2">
-            <h3 className="font-bold">Compañia</h3>
-          </li>
-          <li className="text-xs md:text-base text-gray-900">
-            <a href="/" className="md:hover:underline">
-              Inicio
-            </a>
-          </li>
-          <li className="text-xs md:text-base text-gray-900">
-            <a href="/" className="md:hover:underline">
-              Nosotros
-            </a>
-          </li>
-          <li className="text-xs md:text-base text-gray-900">
-            <a href="/" className="md:hover:underline">
-              Contáctanos
-            </a>
-          </li>
-        </ul>
-
-        <ul className="flex flex-col p-0 gap-2">
-          <li className="text-sm md:text-base mb-2">
-            <h3 className="font-bold">Redes Sociales</h3>
-          </li>
-          <li className="text-xs md:text-base text-gray-900">
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href="https://www.instagram.com/update.ec/"
-              className="md:hover:underline"
-            >
-              Instagram
-            </a>
-          </li>
-        </ul>
+        <div className="text-xs md:text-sm text-black flex gap-2 mt-24 justify-center">
+          <a
+            href="https://github.com/Rysth"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-bold md:hover:-translate-y-2 transition duration-300"
+          >
+            RysthCraft
+            <i className="fa-brands fa-github ml-1" />
+          </a>
+          <p>| Todos los Derechos Reservados</p>
+        </div>
       </div>
     </motion.div>
   );
