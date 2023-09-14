329ae7cc8b5d06678313da65f7354ab2fc816423
Rysth <johnpalacios.t@gmail.com>
2023-09-14 16:31:54 -0500

    Update with the actual API_URL.

--- src/utils/NavigationUtils.js
+++ src/utils/NavigationUtils.js
@@ -1,5 +1,6 @@
 /* URLS */
 export const contactURL = 'https://wa.me/message/LERWWHRHDOYEC1';
+export const API_URL = 'https://strapi-updateec-rysthcraft.koyeb.app';
 
 export const handleNavigation = (route) => {
   window.open(route);
