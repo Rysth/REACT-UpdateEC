2c1c090c4d2c18a366d453f7bd61de2f13eda90f
Rysth <johnpalacios.t@gmail.com>
2023-12-07 10:44:06 -0500

    Create Session SignIn component.

--- src/pages/Session/SignIn.js
+++ src/pages/Session/SignIn.js
@@ -1,12 +1,23 @@
 import { motion } from 'framer-motion';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { createSession } from '../../redux/slices/sessionSlice';
 
 function SignIn() {
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (userData) => {
+    dispatch(createSession(userData));
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 2 }}
     >
+      {/* eslint-disable */}
       <section className="h-screen">
         <div className="flex flex-col sm:grid sm:grid-cols-[55%_1fr] h-full">
           <header className="pt-16 h-full bg-gradient-to-r from-[var(--CL-primary-purple)] to-[var(--CL-primary-cyan)] flex flex-col items-center justify-center text-white gap-1 max-h-40 sm:max-h-full">
@@ -14,25 +25,33 @@ function SignIn() {
             <p className="">e-Commerce</p>
           </header>
           <div className="flex flex-col items-center justify-center h-full text-white">
-            <form className="flex flex-col items-center justify-center h-full gap-1 w-72 sm:rounded-l-3xl">
+            <form
+              className="flex flex-col items-center justify-center h-full gap-1 w-72 sm:rounded-l-3xl"
+              onSubmit={handleSubmit(onSubmit)}
+            >
               <h2 className="w-full mb-5 text-3xl font-bold text-left md:text-4xl">
                 Iniciar Sesión
               </h2>
-              <label htmlFor="email" className="grid w-full gap-2 text-sm">
+              <label htmlFor="identifier" className="grid w-full gap-2 text-sm">
                 <span className="font-semibold">Correo Electrónico:</span>
                 <input
                   type="email"
+                  {...register('identifier', { required: true })}
                   required
-                  className="p-3 px-5 border border-gray-200 rounded-full"
+                  className="p-3 px-5 text-black border border-gray-200 rounded-full"
                   placeholder="username@example.com"
                 />
               </label>
-              <label htmlFor="email" className="grid w-full gap-2 mt-5 text-sm">
+              <label
+                htmlFor="password"
+                className="grid w-full gap-2 mt-5 text-sm"
+              >
                 <span className="font-semibold">Contraseña:</span>
                 <input
                   type="password"
+                  {...register('password')}
                   required
-                  className="p-3 px-5 border border-gray-200 rounded-full"
+                  className="p-3 px-5 text-black border border-gray-200 rounded-full"
                   placeholder="*********"
                 />
               </label>
--- src/redux/slices/sessionSlice.js
+++ src/redux/slices/sessionSlice.js
@@ -1,19 +1,69 @@
-import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { NotificationManager } from 'react-notifications';
+import API_URL from '../../helpers/connection';
 
 const userData = JSON.parse(sessionStorage.getItem('userData'));
-const authToken = JSON.parse(sessionStorage.getItem('authToken'));
+const authToken = sessionStorage.getItem('authToken');
 
 const initialState = {
   userData: userData || {},
-  authToken: authToken || {},
-  loading: {},
+  authToken: authToken === null,
+  loading: false,
+  error: false,
   active: false,
 };
+
+// Sessions - Sign In
+export const createSession = createAsyncThunk(
+  'session/createSession',
+  async (userData) => {
+    try {
+      const response = await axios.post(`${API_URL}/auth/local`, userData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.status !== 200) {
+        NotificationManager.error(
+          'Email/Contraseña Incorrecto.',
+          'Error',
+          1250,
+        );
+        throw new Error('Error creating the session');
+      }
+      NotificationManager.success('Sesión Iniciada', 'Éxito', 1250);
+      return response.data;
+    } catch (error) {
+      NotificationManager.error('Problema de Servidor.', 'Error', 1250);
+      throw new Error(`Error: ${error.message}`);
+    }
+  },
+);
 
 const sessionSlice = createSlice({
   name: 'session',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createSession.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createSession.fulfilled, (state, action) => {
+        state.loading = false;
+        state.userData = action.payload.user;
+        state.authToken = action.payload.jwt;
+        console.log(action.payload.user);
+        sessionStorage.setItem('userData', JSON.stringify(state.userData));
+        sessionStorage.setItem('authToken', state.authToken);
+      })
+      .addCase(createSession.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export const sessionActions = sessionSlice.actions;
