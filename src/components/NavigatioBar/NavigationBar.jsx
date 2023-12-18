3c4ec109eb36f8814fa5f4ca76fc74057c42d0dc
Rysth <johnpalacios.t@gmail.com>
2023-12-18 13:24:16 -0500

    Create Destroy Session.

--- src/components/NavigatioBar/NavigationBar.jsx
+++ src/components/NavigatioBar/NavigationBar.jsx
@@ -1,23 +1,50 @@
 import { Button, Navbar } from 'flowbite-react'
 import BrandImage from '../../assets/SVG/brand.svg'
 import { Link, NavLink } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { sessionActions } from '../../redux/slices/sessionSlice'
 
 function NavigationBar() {
+  const dispatch = useDispatch()
+  const { active } = useSelector((store) => store.session)
+
+  const onDestroySession = () => dispatch(sessionActions.destroySession())
+
   return (
     <Navbar className="sticky top-0 items-center py-3 bg-gray-900">
       <Navbar.Brand href="/">
         <img src={BrandImage} className="w-24 sm:w-32" alt="Flowbite React Logo" />
       </Navbar.Brand>
       <div className="flex items-center gap-2 sm:gap-6 md:order-2">
-        <Navbar.Link
-          href="/sign_in"
-          className="hidden py-2 text-xs text-white cursor-pointer sm:inline-block md:hover:text-cyan-300 md:transition"
-        >
-          Iniciar Sesión
-        </Navbar.Link>
-        <Button gradientDuoTone="purpleToBlue" className="p-2 transition rounded-full" size="xs" as={Link} to="sign_up">
-          Registrarse
-        </Button>
+        {!active ? (
+          <>
+            <Navbar.Link
+              as={NavLink}
+              to="/sign_in"
+              className="hidden py-2 text-xs text-white cursor-pointer sm:inline-block md:hover:text-cyan-300 md:transition"
+            >
+              Iniciar Sesión
+            </Navbar.Link>
+            <Button
+              gradientDuoTone="purpleToBlue"
+              className="p-2 transition rounded-full"
+              size="xs"
+              as={Link}
+              to="/sign_up"
+            >
+              Registrarse
+            </Button>
+          </>
+        ) : (
+          <Button
+            gradientDuoTone="pinkToOrange"
+            className="p-2 transition rounded-full"
+            size="xs"
+            onClick={onDestroySession}
+          >
+            Cerrar Sesión
+          </Button>
+        )}
         <Navbar.Toggle className="text-white active:bg-transparent hover:bg-transparent" />
       </div>
       <Navbar.Collapse>
