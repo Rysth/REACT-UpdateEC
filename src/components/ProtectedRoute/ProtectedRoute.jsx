4dc56b3274a441441b525e319a06f83b655aa86b
Rysth <johnpalacios.t@gmail.com>
2023-12-18 18:02:51 -0500

    Fix Eslint errors.

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,17 +1,17 @@
-import { Navigate } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Navigate } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 function ProtectedRoute({ isAllowed, children, redirectTo }) {
   if (!isAllowed) {
-    return <Navigate to={redirectTo} />;
+    return <Navigate to={redirectTo} />
   }
-  return children;
+  return children
 }
 
 ProtectedRoute.propTypes = {
   isAllowed: PropTypes.bool.isRequired,
   children: PropTypes.element.isRequired,
   redirectTo: PropTypes.string.isRequired,
-};
+}
 
-export default ProtectedRoute;
+export default ProtectedRoute
