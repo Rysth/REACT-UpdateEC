b80153b4f38e4f807733cefe30dbd4cca2b00fd0
Rysth <johnpalacios.t@gmail.com>
2023-12-26 12:30:41 -0500

    Develop HeroSection for HomePage.

--- src/pages/HomePage/sections/HeroSection.jsx
+++ src/pages/HomePage/sections/HeroSection.jsx
@@ -1,7 +1,19 @@
 import HeroImage from '../../../assets/PNG/hero/hero_2.jpg'
 
 function HeroSection() {
-  return <header>Hello</header>
+  return (
+    <section>
+      <article className="max-w-screen-xl mx-auto">
+        <header className="flex flex-col items-center max-w-lg gap-5 py-12 mx-auto text-center sm:py-20">
+          <h2 className="text-3xl font-bold sm:text-4xl md:text-5xl lg:text-7xl">Bienvenidos a UpdateEC</h2>
+          <p className="text-sm text-gray-500 sm:text-base">
+            Optimiza tu experiencia gaming con nuestros componentes de vanguardia. Descubre el rendimiento superior para
+            conquistar cada partida. <span className="font-semibold ">¡Explora ahora!</span>
+          </p>
+        </header>
+      </article>
+    </section>
+  )
 }
 
 export default HeroSection
