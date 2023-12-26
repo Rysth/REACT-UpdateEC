37d34ce1cf6c4d6b874e5280749f4818b1c4cb42
Rysth <johnpalacios.t@gmail.com>
2023-12-26 14:31:29 -0500

    Create DesginSection

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import HeroSection from './sections/HeroSection'
 import FeatureSection from './sections/FeatureSection'
 import ActionSection from './sections/ActionSection'
+import DesignSection from './sections/DesignSection'
 
 function HomePage() {
   return (
@@ -8,6 +9,7 @@ function HomePage() {
       <HeroSection />
       <ActionSection />
       <FeatureSection />
+      <DesignSection />
     </section>
   )
 }
--- src/pages/HomePage/sections/ActionSection.jsx
+++ src/pages/HomePage/sections/ActionSection.jsx
@@ -6,7 +6,7 @@ function ActionSection() {
       <div className="max-w-screen-xl mx-auto">
         <div className="flex flex-col items-center justify-between gap-4 rounded-lg sm:flex-row">
           <header className="text-center sm:text-left">
-            <h2 className="mb-2 text-2xl font-bold sm:mb-1 text-cyan-300 md:text-4xl">Potencia tu Juego</h2>
+            <h2 className="mb-2 text-2xl font-bold sm:mb-1 text-cyan-400 md:text-4xl">Potencia tu Juego</h2>
             <p className="max-w-md text-sm text-gray-200 sm:text-base">
               Domina con estilo. Encuentra tus herramientas gamer. ¡Explora y juega mejor!
             </p>
