aaf3dfb728e2e7587a43c7d1f0dc87f4cbaaee70
Rysth <johnpalacios.t@gmail.com>
2024-01-11 11:25:52 -0500

    Improve UX in Order and Carts.

--- src/pages/OrderPage/OrderPage.jsx
+++ src/pages/OrderPage/OrderPage.jsx
@@ -1,15 +1,17 @@
-import { Badge, Table, TextInput } from 'flowbite-react'
+import { Badge, Button, Table, TextInput } from 'flowbite-react'
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import BreadCrumb from '../../components/navigation/BreadCrumb/BreadCrumb'
 import SectionLayout from '../../layouts/SectionLayout'
 import { fetchOrders, orderActions } from '../../redux/slices/orderSlice'
 import debounce from 'lodash/debounce'
+import { HiOutlineShoppingCart, HiArrowLeft, HiDocumentSearch } from 'react-icons/hi'
+import LoadingCard from '../../components/ui/LoadingCard/LoadingCard'
 
 function OrderPage() {
   const dispatch = useDispatch()
   const { userData } = useSelector((store) => store.session)
-  const { ordersArray } = useSelector((store) => store.order)
+  const { ordersArray, loading } = useSelector((store) => store.order)
   const [searchData, setSearchData] = useState('')
 
   // Debounced search handler
@@ -32,6 +34,25 @@ function OrderPage() {
       debouncedSearch.cancel()
     }
   }, [debouncedSearch])
+
+  if (loading) {
+    return <LoadingCard />
+  }
+
+  if (ordersArray.length === 0 && loading) {
+    return (
+      <header className="flex flex-col items-center justify-center w-full h-[100vh] max-w-screen-xl gap-2 py-2 mx-auto place-items-center">
+        <HiDocumentSearch className="text-8xl" />
+        <h3 className="w-full text-lg font-bold text-center text-gray-900 uppercase sm:text-2xl ">
+          ¡No tienes Ordenes Aún!
+        </h3>
+        <Button href="/shop" color="blue" size="sm">
+          <HiArrowLeft className="mr-1" />
+          Regresar
+        </Button>
+      </header>
+    )
+  }
 
   return (
     <section>
@@ -59,9 +80,10 @@ function OrderPage() {
             <Table hoverable>
               <Table.Head>
                 <Table.HeadCell className="min-w-[10rem]">Fecha</Table.HeadCell>
-                <Table.HeadCell>Estado</Table.HeadCell>
                 <Table.HeadCell>Factura ID</Table.HeadCell>
+                <Table.HeadCell className="min-w-[10rem]">Estado Envío</Table.HeadCell>
                 <Table.HeadCell className="min-w-[10rem]">Método de Pago</Table.HeadCell>
+                <Table.HeadCell>Estado Pago</Table.HeadCell>
                 <Table.HeadCell>Total</Table.HeadCell>
               </Table.Head>
               <Table.Body className="divide-y">
@@ -74,6 +96,19 @@ function OrderPage() {
                   return (
                     <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800" key={index}>
                       <Table.Cell className="font-medium text-gray-900">{order.attributes.date}</Table.Cell>
+                      <Table.Cell>{paymentID}</Table.Cell>
+                      <Table.Cell>
+                        {true ? (
+                          <Badge color="blue" className="max-w-max">
+                            Completo
+                          </Badge>
+                        ) : (
+                          <Badge color="indigo" className="max-w-max">
+                            Pendiente
+                          </Badge>
+                        )}
+                      </Table.Cell>
+                      <Table.Cell>PayPal</Table.Cell>
                       <Table.Cell>
                         {isCompleted ? (
                           <Badge color="success" className="max-w-max">
@@ -85,9 +120,7 @@ function OrderPage() {
                           </Badge>
                         )}
                       </Table.Cell>
-                      <Table.Cell>{paymentID}</Table.Cell>
-                      <Table.Cell>PayPal</Table.Cell>
-                      <Table.Cell className="font-medium text-green-600">${paymentTotal}</Table.Cell>
+                      <Table.Cell className="font-bold text-green-600">${paymentTotal}</Table.Cell>
                     </Table.Row>
                   )
                 })}
